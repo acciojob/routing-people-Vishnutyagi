a7cee45424c8ccd4771301245bfdbe6adc9cf1ae
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-07 13:29:09 +0530

    okk?

--- src/components/users.js
+++ src/components/users.js
@@ -3,10 +3,10 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const items=[
-  { id: 1, name: 'Leanne Graham', description: 'Description for Item 1',username: "Bret",email:"Sincere@april.biz" },
-  { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' ,username: "Antonette",email:"Sincere@april.biz" },
-  { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3',username: "Samantha",email:"Sincere@april.biz"  },
-  { id: 3, name: 'Patricia Labsack', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz" }
+  { id: 1, name: 'Leanne Graham', description: 'Description for Item 1',username: "Bret",email:"Sincere@april.biz" ,Phone: "1-770-736-8031 x56442",Website: "hildegard.org"},
+  { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' ,username: "Antonette",email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
+  { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3',username: "Samantha",email:"Sincere@april.biz" ,Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
+  { id: 3, name: 'Patricia Labsack', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" }
 ];
 
 function Users() {
@@ -28,6 +28,8 @@ function Users() {
         <p>Name: {item.name}</p>
         <p>Username: {item.username} </p>
         <p>Email: {item.email} </p>
+        <p>Phone: {item.Phone} </p>
+        <p>Website: {item.Website}</p>
       </div>
     )}</div>
   )
