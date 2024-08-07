7bd30cd3f0e24b4090ec441d84fc03c17b89a1ab
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-07 13:36:31 +0530

    okk?

--- src/components/home.js
+++ src/components/home.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const items=[
-  { id: 1, name: 'Leanne Graham', description: 'Description for Item 1' },
-  { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' },
-  { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3' },
-  { id: 3, name: 'Patricia Labsack', description: 'Description for Item 3' },
-  { id: 3, name: 'Item 1', description: 'Description for Item 1' },
-  { id: 3, name: 'Item 2', description: 'Description for Item 2' },
-  { id: 3, name: 'Item 3', description: 'Description for Item 3' }
+  { id: 1, name: 'Leanne Graham', description: 'Description for Item 1',username: "Bret",email:"Sincere@april.biz" ,Phone: "1-770-736-8031 x56442",Website: "hildegard.org"},
+  { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' ,username: "Antonette",email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
+  { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3',username: "Samantha",email:"Sincere@april.biz" ,Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
+  { id: 4, name: 'Patricia Labsack', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
+  { id: 5, name: 'Clementina DuBuque', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" }
 ];
 
 function Home() {
