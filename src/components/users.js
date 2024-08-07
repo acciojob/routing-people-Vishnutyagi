9b53e8ab2c85b8c815e5a131db528ff4295b2964
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-07 13:22:38 +0530

    okk?

--- src/components/users.js
+++ src/components/users.js
@@ -3,10 +3,10 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const items=[
-  { id: 1, name: 'Leanne Graham', description: 'Description for Item 1' },
-  { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' },
-  { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3' },
-  { id: 3, name: 'Patricia Labsack', description: 'Description for Item 3' },
+  { id: 1, name: 'Leanne Graham', description: 'Description for Item 1',username: "Bret" },
+  { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' ,username: "Antonette" },
+  { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3',username: "Samantha"  },
+  { id: 3, name: 'Patricia Labsack', description: 'Description for Item 3',username: "Bret"  },
   { id: 3, name: 'Item 1', description: 'Description for Item 1' },
   { id: 3, name: 'Item 2', description: 'Description for Item 2' },
   { id: 3, name: 'Item 3', description: 'Description for Item 3' }
@@ -29,6 +29,7 @@ function Users() {
       <div>
         <h1>User Details</h1>
         <p>Name: {item.name}</p>
+        <p>Username: {item.username} </p>
       </div>
     )}</div>
   )
