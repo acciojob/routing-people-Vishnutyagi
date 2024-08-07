87d1e175986f16e1da16be5967d0ce0e8699d6ab
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-07 13:32:00 +0530

    okk?

--- src/components/users.js
+++ src/components/users.js
@@ -6,7 +6,8 @@ const items=[
   { id: 1, name: 'Leanne Graham', description: 'Description for Item 1',username: "Bret",email:"Sincere@april.biz" ,Phone: "1-770-736-8031 x56442",Website: "hildegard.org"},
   { id: 2, name: 'Ervin Howell', description: 'Description for Item 2' ,username: "Antonette",email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
   { id: 3, name: 'Clenmentine Bouch', description: 'Description for Item 3',username: "Samantha",email:"Sincere@april.biz" ,Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
-  { id: 3, name: 'Patricia Labsack', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" }
+  { id: 4, name: 'Patricia Labsack', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" },
+  { id: 5, name: 'Clementina DuBuque', description: 'Description for Item 3',username: "Bret" ,email:"Sincere@april.biz",Phone: "1-770-736-8031 x56442",Website: "hildegard.org" }
 ];
 
 function Users() {
